8c5cad4459dbc5efcba4a3413ab008bb56443501
nbaev <nikita_uray@mail.ru>
2025-11-17 14:59:02 +0300

    fix: config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,28 +7,36 @@ import (
 )
 
 type Config struct {
-	DB_PORT string
-	DB_USER string
-	DB_PASS string
-	DB_HOST string
-	DB_NAME string
-
+	DB_PORT  string
+	DB_USER  string
+	DB_PASS  string
+	DB_HOST  string
+	DB_NAME  string
 	SER_PORT string
 }
 
 func Load() *Config {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	cfg := Config{
-		DB_PORT:  os.Getenv("DB_PORT"),
-		DB_USER:  os.Getenv("DB_USER"),
-		DB_PASS:  os.Getenv("DB_PASS"),
-		DB_HOST:  os.Getenv("DB_HOST"),
-		DB_NAME:  os.Getenv("DB_NAME"),
-		SER_PORT: os.Getenv("SER_PORT"),
-	}
-	return &cfg
 
+	_ = godotenv.Load(".env")
+
+	cfg := Config{
+		DB_PORT:  getEnv("DB_PORT", "5432"),
+		DB_USER:  getEnv("DB_USER", "postgres"),
+		DB_PASS:  getEnv("DB_PASS", "password"),
+		DB_HOST:  getEnv("DB_HOST", "postgres"),
+		DB_NAME:  getEnv("DB_NAME", "postgres"),
+		SER_PORT: getEnv("SER_PORT", "8088"),
+	}
+
+	log.Printf("Loaded config: DB_HOST=%s, DB_PORT=%s, SER_PORT=%s",
+		cfg.DB_HOST, cfg.DB_PORT, cfg.SER_PORT)
+
+	return &cfg
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
